c3df4a2541186b0f49eda26c988a7d6bde53d95e
Olesya Asanova <asa88.88@bk.ru>
2022-09-05 17:50:04 +0300

    bonus homework

---
+++ app.js
@@ -0,0 +1,26 @@
+async function getResponse() {
+    let response = await fetch('dbHeroes.json')
+    let content = await response.json()
+    content = content.splice(0, 28)
+
+    let list = document.querySelector('.cards')
+    let key
+
+    for (key in content) {
+        list.innerHTML += `
+        <div class="card">
+        <img src="${content[key].photo}" width="288">
+        <div class="card-body">
+                <h3>${content[key].name}</h3>
+                <h4>${content[key].realName}</h4>
+                  <p class="card-text">
+                  ${content[key].status}
+                  <h5>${content[key].actors}</h5>
+                  ${content[key].movies}
+                  </p>
+                </div>
+        </div>
+        `
+    }
+}
+getResponse()
