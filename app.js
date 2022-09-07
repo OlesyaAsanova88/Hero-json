da45f31eb4ad8f94f8d9e1856329b98d7574dae5
Olesya <asa88.88@bk.ru>
2022-09-07 12:31:03 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,37 +1,34 @@
 async function getResponse() {
-<<<<<<< HEAD
+
   let response = await fetch('dbHeroes.json')
   let content = await response.json()
   content = content.splice(0, 50)
-=======
+  
     let response = await fetch('dbHeroes.json')
     let content = await response.json()
     content = content.splice(0, 50)
->>>>>>> 00fd5b1339169771cd00395cef6504876d14199d
 
   let list = document.querySelector('.cards')
   let key
 
   for (key in content) {
     list.innerHTML += `
-    <div class="col-lg-3 col-md-6 col-sm-12 mb-3">
-      <div class=cards-block>
-        <div class="card">
-          <img src="${content[key].photo}" width="260">
-          <div class="card-body">
-            <h3>${content[key].name}</h3>
-            <h4>${content[key].realName}</h4>
-            <p class="card-text">
-            ${content[key].status}
-            <h5>${content[key].actors}</h5>
-            ${content[key].movies}
-            </p>
+        <div class="col-lg-3 col-md-6 col-sm-12 mb-3">
+          <div class=cards-block>
+            <div class="card">
+              <img src="${content[key].photo}" width="260">
+              <div class="card-body">
+                <h3>${content[key].name}</h3>
+                <h4>${content[key].realName}</h4>
+                <p class="card-text">
+                ${content[key].status}
+                <h5>${content[key].actors}</h5>
+                ${content[key].movies}
+                </p>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
-        
-    
         `
   }
 }
